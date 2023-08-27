41eb3755146cffa9d8e30241bc4396ec54a79837
Frank Smith <fasvi6@gmail.com>
2023-08-27 12:56:19 -0400

    Blank slate, fresh start!

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 This is an old dice game I discovered once when looking for personal project ideas. I then made it into a web dev project and host it here: franksmith22.github.io/PigDiceGame.
 
 I would now like to remake it in React to keep up practice!
+
+
+To keep myself motivated and organized, I will do the following
+1. Make a trello board to keep track of individual features so it's not just all up in my head
+2. Not get hung up on the design. The difficult part for me here is the data side of things with react + redux, so focus on that first. Design will come later.
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,74 +1,18 @@
-import React, { useState } from "react";
-import { Button, Col, Container, Modal, ModalBody, ModalHeader, Row } from "reactstrap";
-import PlayerLabel from "./PlayerLabel";
-import DieFace from "./DieFace";
+import React from "react";
 
 const Main = () => {
 
-    const [startModal, setStartModal] = useState(true)
-    const [playerNames, setPlayerNames] = useState({ playerOne: "", playerTwo: "" })
-
-    const toggleModal = () => setStartModal(!startModal)
-
-    const handleNameFields = (playerNum, text) => {
-        switch(playerNum){
-            case 1: setPlayerNames({...playerNames, playerOne: text})
-                    break
-            case 2: setPlayerNames({...playerNames, playerTwo: text})
-        }
-    } 
 
     return (
         <>
-            <Modal isOpen={startModal} toggle={toggleModal}>
-                <ModalHeader toggle={toggleModal}>Welcome to Pig!</ModalHeader>
-                <ModalBody>
-                    <Row>
-                        <Col>
-                            <p className="pb-3">
-                                <b>How the game works:</b><br />
-                                Two players take turns rolling a dice. Whatever number shows is added to that players <b>turn total</b>.
-                                That player can then decide to roll again, or hold their points to add to the <b>overall total</b> and switch the turn back to the other player.
-                                Why hold points? If the dice rolls a 1, your turn ends and you lose your <b>turn total</b> points!
-                            </p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <label for="playerOneField">Player One</label><br />
-                            <input 
-                                id="playerOneField" 
-                                value={playerNames.playerOne} 
-                                type="text" 
-                                onChange={(e)=>{handleNameFields(1, e.target.value)}}
-                                className="mb-4"
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <label for="playerTwoField">Player Two</label><br />
-                            <input 
-                                id="playerTwoField" 
-                                value={playerNames.playerTwo} 
-                                type="text" 
-                                onChange={(e)=>{handleNameFields(2, e.target.value)}}
-                                className="mb-4"
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Button onClick={() => setStartModal(false)}>Start Game</Button>
-                        </Col>
-                    </Row>
-
-                </ModalBody>
-            </Modal>
-            <Container>
-                <PlayerLabel playerNames={playerNames} />
-                <DieFace faceNum={3}/>
-            </Container>
+            <div>
+                <p>
+                Player name: Frank <br />
+                total score: 50 <br />
+                current score: 3 <br />
+                </p>
+            </div>
+            <button>Roll</button>
         </>
     )
 }
