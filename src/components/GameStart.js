8aa9bbe6a5c3715739dc913fa6bee55d5fe28255
Frank Smith <fasvi6@gmail.com>
2023-10-01 15:10:40 -0400

    Implement name randomizer

--- src/components/GameStart.js
+++ src/components/GameStart.js
@@ -8,6 +8,8 @@ import {
     Col
 } from 'reactstrap'
 import { setPlayers } from '../features/players/playersSlice'
+import { NAMES } from '../app/nameGeneration/names'
+import { ADJECTIVES } from '../app/nameGeneration/adjectives'
 
 const GameStart = () => {
 
@@ -20,6 +22,18 @@ const GameStart = () => {
     const handleGameStart = () => {
         dispatch(setPlayers({playerNames: [playerOneName, playerTwoName]}))
         setIsOpen(false)
+    }
+
+    const randomizeName = (playerNum) => {
+        const randomName = NAMES[Math.floor(Math.random() * NAMES.length)]
+        let randomAdjective = ADJECTIVES[Math.floor(Math.random() * ADJECTIVES.length)]
+
+        randomAdjective = randomAdjective.charAt(0).toUpperCase() + randomAdjective.slice(1)
+
+        switch(playerNum){
+            case 1: setPlayerOneName(`${randomAdjective} ${randomName}`); break;
+            case 2: setPlayerTwoName(`${randomAdjective} ${randomName}`); break;
+        }
     }
 
     return (
@@ -35,7 +49,7 @@ const GameStart = () => {
                         <input id='playerOne' value={playerOneName} className='form-control' type='text' onChange={e => setPlayerOneName(e.target.value)}/>
                     </Col>
                     <Col xs='2'>
-                        <button className='btn'><i className='fa fa-random'></i></button>
+                        <button className='btn' onClick={() => randomizeName(1)}><i className='fa fa-random'></i></button>
                     </Col>
                 </Row>
                 <label className='mt-3' htmlFor='playerTwo'>Player 2</label><br/>
@@ -44,7 +58,7 @@ const GameStart = () => {
                         <input id='playerTwo' value={playerTwoName} className='form-control' type='text' onChange={e => setPlayerTwoName(e.target.value)}/>
                     </Col>
                     <Col xs='2'>
-                        <button className='btn'><i className='fa fa-random'></i></button>
+                        <button className='btn' onClick={() => randomizeName(2)}><i className='fa fa-random'></i></button>
                     </Col>
                 </Row>
                 <Row className="mt-5">
